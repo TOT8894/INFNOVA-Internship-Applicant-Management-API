a4f3ebcfc5fc3e1268cc8d98389e76d0938ce70c
shanbel <shanbeldires7@gmail.com>
2026-07-18 19:39:35 +0300

    import modules to app module from different modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { ApplicantsModule } from './applicants/applicants.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, ApplicantsModule, DashboardModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
