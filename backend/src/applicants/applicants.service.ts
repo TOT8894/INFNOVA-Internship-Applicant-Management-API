ab5b967c8fc56fbbfd1306077b19dc132c7e2783
shanbel <shanbeldires7@gmail.com>
2026-07-19 11:48:33 +0300

    adding applicant service functions to create update and delete the applicant and also  business logics

--- backend/src/applicants/applicants.service.ts
+++ backend/src/applicants/applicants.service.ts
@@ -1,4 +1,54 @@
 import { Injectable } from '@nestjs/common';
+import { UpdateApplicantDto } from './dto/update-applicant.dto';
+import { CreateApplicantDto } from './dto/create-applicant.dto';
 
 @Injectable()
-export class ApplicantsService {}
+export class ApplicantsService {
+    private Applicant=[
+        {id:124,name:"abebe", email:"abc"},
+        {id:125,name:"adane", email:"abcd"},
+        {id:126,name:"gebre", email:"abcde"},
+    ]
+    getallApplicant(){
+        return this.Applicant
+    }
+
+    getApplicantByQuery(name:string,email:string,id:number){ 
+        let result = this.Applicant
+        if(name){
+            result =  result.filter((Applicant:any)=>(Applicant.name.toLowerCase().includes(name.toLowerCase())))
+        }
+        if(email){
+            result =  result.filter((Applicant:any)=>(Applicant.email.toLowerCase().includes(email.toLowerCase())))
+        }
+        if(id){
+            result =  result.filter(Applicant=>Applicant.id==id)
+        }
+        return result
+    }
+
+    getApplicantById(id:number){
+        let applicant = this.Applicant.find(Applicant=>Applicant.id==id)
+        return applicant
+    }
+    createApplicant(createApplicantDto:CreateApplicantDto){
+        this.Applicant.push(createApplicantDto)
+        return {data:createApplicantDto,message:"applicant created successfuly"}
+    }
+    
+    updateApplicant(id:number,updateApplicantDto:UpdateApplicantDto){
+         let index = this.Applicant.findIndex(Applicant=>Applicant.id==id)
+        this.Applicant[index]={
+            ...this.Applicant[index],
+            ...updateApplicantDto
+        }
+        
+        return {data:this.Applicant[index],message:`${id} this Applicant updated`}
+    }
+
+    deleteApplicant(id:number){
+        let index = this.Applicant.findIndex(Applicant=>Applicant.id==id)
+        this.Applicant.splice(index,1)
+        return {message:`${id} this Applicant deleted`}
+    }
+}
