ac782b7f40b51012cd13de0d497292f6080a2aaa
shanbel <shanbeldires7@gmail.com>
2026-07-19 17:55:22 +0300

    defining applicants information validation for input and to validate

--- backend/src/applicants/dto/create-applicant.dto.ts
+++ backend/src/applicants/dto/create-applicant.dto.ts
@@ -1,7 +1,43 @@
-export class CreateApplicantDto{
-    id:number;
-    name:string;
-    email:string; 
-    notes:string;
-    track:string;  
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { InternshipTrack } from 'src/generated/prisma/enums';
+
+export class CreateApplicantDto {
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  name: string;
+
+  @ApiProperty()
+  @IsEmail()
+  @IsNotEmpty()
+  @MinLength(5)
+  email: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  @MinLength(10)
+  phone?: string;
+
+  @ApiProperty({
+    enum: InternshipTrack,
+  })
+  @IsEnum(InternshipTrack)
+  track: InternshipTrack;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  @MaxLength(1000)
+  notes?: string;
 }
