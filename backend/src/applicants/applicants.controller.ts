9f25d7d15302ab0e62f0f3fff732020641c8af8a
shanbel <shanbeldires7@gmail.com>
2026-07-19 17:52:42 +0300

    modularizing  the function and add updage notes and status functions

--- backend/src/applicants/applicants.controller.ts
+++ backend/src/applicants/applicants.controller.ts
@@ -1,42 +1,98 @@
-import { Controller, Get, Post, Put, Delete, Query, Param, Body } from '@nestjs/common';
+import { 
+    Controller, 
+    Get, 
+    Post,  
+    Delete, 
+    Query, 
+    Param, 
+    Body, 
+    Patch, 
+} from '@nestjs/common';
+
 import { ApplicantsService } from './applicants.service';
 import { CreateApplicantDto } from './dto/create-applicant.dto';
 import { UpdateApplicantDto } from './dto/update-applicant.dto';
+import { UpdateStatusDto } from './dto/update-status.dto';
+import { UpdateNotesDto } from './dto/update-notes.dto';
+
+
 @Controller('applicants')
 export class ApplicantsController {
-    constructor(private readonly applicantService:ApplicantsService){}
-        
+    constructor(
+        private readonly applicantService:ApplicantsService
+    ){}
+    
+    
     @Get("all")
     getallApplicant(){
     return this.applicantService.getallApplicant()
     }
 
+
     @Get()
-    getApplicant(
+    getApplicantByQuery(
         @Query('name') name:string,
         @Query('email') email:string,
-        @Query('id')id:number
+        @Query('id') id:string
     ){
         return this.applicantService.getApplicantByQuery(name,email,id)
-   }
+    }
    
+
     @Post()
-    createApplicant(@Body() createApplicantDto:CreateApplicantDto){
+    createApplicant(
+        @Body() createApplicantDto:CreateApplicantDto
+    ){
         return this.applicantService.createApplicant(createApplicantDto) 
     } 
 
+
     @Get(":id")
-    getApplicantById(@Param('id') id:number){
+    getApplicantById(
+        @Param('id') id:string
+    ){
       return this.applicantService.getApplicantById(id)
     }
 
+
     @Delete("/:id")
-    deleteApplicant(@Param('id') id:number){
+    deleteApplicant(
+        @Param('id') id:string
+    ){
        return  this.applicantService.deleteApplicant(id)
     }
-    @Put("/:id")
-    updateApplicant(@Param('id')id:number,@Body() updateApplicantDto:UpdateApplicantDto){
+
+
+    @Patch("/:id")
+    updateApplicant(
+        @Param('id') id:string,
+        @Body() updateApplicantDto:UpdateApplicantDto
+    ){
        return this.applicantService.updateApplicant(id,updateApplicantDto)
+    }
+
+
+    @Patch(':id/status')
+    updateStatus(
+        @Param('id') id: string,
+        @Body() updateStatusDto: UpdateStatusDto,
+    ) {
+        return this.applicantService.updateStatus(
+        id,
+        updateStatusDto,
+        );
+    }
+
+
+    @Patch(':id/notes')
+    updateNotes(
+        @Param('id') id: string,
+        @Body() updateNotesDto: UpdateNotesDto,
+    ) {
+        return this.applicantService.updateNotes(
+        id,
+        updateNotesDto,
+        );
     }
 }
 
