13dea5364e1709eb7b35d9f2c10f01abfda09a13
shanbel <shanbeldires7@gmail.com>
2026-07-21 19:35:26 +0300

    import additional modules and make them ready to be used other places

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,9 +1,35 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [
+    PrismaModule,
+    PassportModule,
+
+    JwtModule.register({
+      secret: process.env.ACCESS_KEY!,
+      signOptions: {
+        expiresIn: process.env.ACCESS_EXPIRE_DATE as any,
+      },
+    }),
+  ],
+
   controllers: [AuthController],
-  providers: [AuthService]
+
+  providers: [
+    AuthService,
+    JwtStrategy, 
+  ],
+
+  exports: [
+    JwtModule,
+    PassportModule,
+  ],
 })
 export class AuthModule {}
