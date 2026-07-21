6d7627b987d067d528fa3b3f04de3cec24631c34
shanbel <shanbeldires7@gmail.com>
2026-07-21 19:33:13 +0300

    adding role based to access the dashboard

--- backend/src/dashboard/dashboard.controller.ts
+++ backend/src/dashboard/dashboard.controller.ts
@@ -1,7 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get,UseGuards } from '@nestjs/common';
 import { DashboardService } from './dashboard.service';
-
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth ,ApiTags} from '@nestjs/swagger'
+@ApiTags('dashboard')
 @Controller('dashboard')
+@ApiBearerAuth('access-token')
+@UseGuards(AuthGuard('jwt'))
 export class DashboardController {
     constructor(
         private readonly dashBoardService:DashboardService
