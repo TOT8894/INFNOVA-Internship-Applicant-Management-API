0309a0cabdc03fa3c814921b277c79286bcc838b
shanbel <shanbeldires7@gmail.com>
2026-07-22 16:53:41 +0300

    add applicant query dto file

---
+++ backend/src/applicants/dto/applicant-query.dto.ts
@@ -0,0 +1,52 @@
+import { Type } from 'class-transformer';
+import {
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+import {
+  ApplicantStatus,
+  InternshipTrack,
+} from 'src/generated/prisma/enums';
+
+export class ApplicantQueryDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  email?: string;
+
+  @IsOptional()
+  @IsEnum(ApplicantStatus)
+  status?: ApplicantStatus;
+
+  @IsOptional()
+  @IsEnum(InternshipTrack)
+  track?: InternshipTrack;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  page?: number = 1;
+
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  @Max(100)
+  limit?: number = 10;
+
+  @IsOptional()
+  @IsString()
+  sortBy?: string;
+
+  @IsOptional()
+  @IsEnum(['asc', 'desc'])
+  sortOrder?: 'asc' | 'desc' = 'desc';
+}
