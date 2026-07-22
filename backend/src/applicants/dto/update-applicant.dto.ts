3376ada33acf83e6eac31129117ffedb7fd5f163
shanbel <shanbeldires7@gmail.com>
2026-07-22 16:54:37 +0300

    creating update applicant dto instead of partial type inheritance

--- backend/src/applicants/dto/update-applicant.dto.ts
+++ backend/src/applicants/dto/update-applicant.dto.ts
@@ -1,3 +1,35 @@
-import { CreateApplicantDto } from "./create-applicant.dto";
-import {PartialType} from "@nestjs/mapped-types"
-export class UpdateApplicantDto extends PartialType(CreateApplicantDto){}
+import {
+  IsEmail,
+  IsEnum,
+  IsOptional,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+
+import {
+  ApplicantStatus,
+  InternshipTrack,
+} from 'src/generated/prisma/enums';
+
+export class UpdateApplicantDto {
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsEnum(InternshipTrack)
+  track?: InternshipTrack;
+
+  @IsOptional()
+  @IsEnum(ApplicantStatus)
+  status?: ApplicantStatus;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(1000)
+  notes?: string;
+}
